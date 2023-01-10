b71d6bc5b918bfdccadd3ceb523bfe787c9566fc
Inerizal <inerizal@gmail.com>
2023-01-10 11:17:27 +0300

    basic functions done

--- main.js
+++ main.js
@@ -1,72 +1,84 @@
 import './style.css'
 
 const star1Btn = document.querySelector('#star1');
-const mineBtn = document.querySelector('#mine');
+const star2Btn = document.querySelector('#star2');
 const visitBtn = document.querySelector('#visit');
 const mainBtn = document.querySelector('#visitMain');
 
-const buyAutoChopBtn = document.querySelector('#autoChopper');
-const buyPickaxeBtn = document.querySelector('#buyPickaxe');
+const star1BoostBtn = document.querySelector('#star1Boost');
+const star2BoostBtn = document.querySelector('#star2Boost');
 
 const menus = Array.from(document.querySelector('.menus').children);
 
 let star1Power = 0;
 let star1Limit = 100;
 let star1Gain = 10;
-let stones = 0;
-let money = 0;
-let pickaxes = 0;
-let logsPlus = 1;
-let stonePlus = 1;
-let autoLogPlus = 0;
-let autoLogPrice = 100;
-let pickaxePrice = 50;
-let star1Price = 1;
-let stonePrice = 5;
-let curMenu;
+let star2Power = 0;
+let star2Limit = 1000;
+let star2Gain = 150;
 
-star1Btn.addEventListener('click', chop);
-mineBtn.addEventListener('click', mine);
+let money = 1000;
+let star1Boosts = 0;
+let star1BoostPower = 300;
+let star1BoostPrice = 50;
+
+let star2Boosts = 0;
+let star2BoostPower = 50;
+let star2BoostPrice = 100;
+let star1Price = 1;
+let star2Price = 5;
+
+let curMenu;
+let curIntervalId;
+
+// star1Btn.addEventListener('click', chop);
+star2Btn.addEventListener('click', powerStar2);
 visitBtn.addEventListener('click', visitMarket);
 mainBtn.addEventListener('click', visitMain);
-buyAutoChopBtn.addEventListener('click', buyAutoChop);
-buyPickaxeBtn.addEventListener('click', buyPickaxe);
+star1BoostBtn.addEventListener('click', buyStar1Boost);
+star2BoostBtn.addEventListener('click', buyStar2Boost);
 
-setInterval(() => {
+curIntervalId = startInterval();
+
+function startInterval(time = 1000) {
+    setInterval(() => {
+        star1Power += star1Gain;
+        if (star1Power >= star1Limit) {
+            star1Power = star1Power - star1Limit;
+            money += star1Price;
+        }
+        changeInventory();
+        changeMarket();
+    
+        star1Btn.innerHTML = `${star1Power}/${star1Limit}`;
+    }, time);
+}
+
+function powerStar1() {
     star1Power += star1Gain;
-    if (star1Power === star1Limit) {
-        star1Power = 0;
-        money += star1Price;
+        if (star1Power >= star1Limit) {
+            star1Power = star1Power - star1Limit;
+            money += star1Price;
+        }
+        changeInventory();
+        changeMarket();
+}
+
+function powerStar2() {
+    star2Power += star2Gain + star2BoostPower * star2Boosts;
+    if (star2Power >= star2Limit) {
+        star2Power = star2Power - star2Limit;
+        money += star2Price;
     }
     changeInventory();
     changeMarket();
 
-    star1Btn.innerHTML = `${star1Power}/${star1Limit}`;
-}, 500);
-
-
-// function update() {
-//     star1Btn.innerHTML = `${(star1Power + star1Gain)/60}/${star1Limit}`;
-//     // console.log(star1Power);
-//     requestAnimationFrame(update);
-// } 
-
-// update();
-
-function chop() {
-    logs += logsPlus;
-    changeInventory();
-    changeMarket();
-}
-
-function mine() {
-    stones += stonePlus;
-    changeInventory();
-    changeMarket();
+    star2Btn.innerHTML = `${star2Power}/${star2Limit}`;
 }
 
 function changeInventory() {
     document.querySelector('#money').innerHTML = `Money: $${money}`;
+    console.log('inventory')
 }
 
 function visitMarket() {
@@ -80,51 +92,39 @@ function visitMain() {
 function switchMenu(curMenu) {
     menus.forEach(menu => {
         if (!menu.classList.contains('hidden')) menu.classList.add('hidden');
-        if (menu.classList.contains(curMenu)) menu.classList.remove('hidden');
+        if (menu.classList.contains(curMenu) || menu.classList.contains('inventory')) menu.classList.remove('hidden');
     })
 }
 
 function changeMarket() {
-    if (money >= 100) {
-        buyAutoChopBtn.classList.remove('hidden');
-    }
-    else {
-        buyAutoChopBtn.classList.add('hidden');
-    }
-
     if (money >= 50) {
-        buyPickaxeBtn.classList.remove('hidden');
+        star1BoostBtn.classList.remove('hidden');
     }
     else {
-        buyPickaxeBtn.classList.add('hidden');
+        star1BoostBtn.classList.add('hidden');
+    }
+
+    if (money >= 100) {
+        star2BoostBtn.classList.remove('hidden');
+    }
+    else {
+        star2BoostBtn.classList.add('hidden');
     }
 }
 
-// function sellLogs(logQt = logs) {
-//     logs -= logQt;
-//     money += logQt * logPrice;
-//     changeInventory();
-//     changeMarket();
-// }
-
-// function sellStones(stoneQt = stones) {
-//     stones -= stoneQt;
-//     money += stoneQt * stonePrice;
-//     changeInventory();
-//     changeMarket();
-// }
-
-function buyAutoChop() {
-    autoLogPlus++;
-    money -= autoLogPrice;
+function buyStar1Boost() {
+    star1Boosts++;
+    money -= star1BoostPrice;
     changeInventory();
     changeMarket();
+
+    curIntervalId = null;
+    curIntervalId = startInterval(1000 - star1BoostPower * star1Boosts);
 }
 
-function buyPickaxe() {
-    pickaxes++;
-    money -= pickaxePrice;
+function buyStar2Boost() {
+    star2Boosts++;
+    money -= star2BoostPrice;
     changeInventory();
     changeMarket();
-    mineBtn.classList.remove('hidden');
 }
